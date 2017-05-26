a8b9a982e00e80ad8c12c6055f33bfa3255da44f
Kristin Ottofy <kottofy@outlook.com>
2017-05-25 20:04:36 -0400

    adding sample confluence search bot

---
+++ confluence-search-bot/app.js
@@ -0,0 +1,72 @@
+// This loads the environment variables from the .env file
+require('dotenv-extended').load();
+
+var builder = require('botbuilder');
+var restify = require('restify');
+var Search = require('./search');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+// Create connector and listen for messages
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+server.post('/api/messages', connector.listen());
+
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
+});
+
+// You can provide your own model by specifing the 'LUIS_MODEL_URL' environment variable
+// This Url can be obtained by uploading or creating your model from the LUIS portal: https://www.luis.ai/
+var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
+bot.recognizer(recognizer);
+
+bot.dialog('Search', [
+    function (session, args, next) {
+
+        var searchEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'SearchTerm');
+
+        if (searchEntity) {
+            var searchTerm = searchEntity.entity;
+            session.send("Great! I am searching for " + searchTerm);
+
+            Search.search(searchTerm)
+                .then(function (results) {
+                    var message = new builder.Message()
+                        .attachmentLayout(builder.AttachmentLayout.carousel)
+                        .attachments(results.map(ResultAsAttachment));
+
+                    session.send(message);
+
+                    session.endDialog();
+                });
+        }
+    }
+]).triggerAction({
+    matches: 'Search',
+    onInterrupted: function (session) {
+        session.send('Please provide a search term');
+    }
+});
+
+bot.dialog('Help', function (session) {
+    session.endDialog('Hi! Try asking me things like \'search hotels in Seattle\', \'search hotels near LAX airport\' or \'show me the reviews of The Bot Resort\'');
+}).triggerAction({
+    matches: 'Help'
+});
+
+function ResultAsAttachment(result) {
+    return new builder.HeroCard()
+        .title(result.title)
+        .buttons([
+            new builder.CardAction()
+                .title('More details')
+                .type('openUrl')
+                .value(process.env.CONFLUENCE_SITE_URL + "/wiki/rest/api/content/" + result.id)
+        ]);
+}
---
+++ confluence-search-bot/search.js
@@ -0,0 +1,32 @@
+var request = require('request');
+var Promise = require('bluebird');
+
+module.exports = {
+    search: function (searchTerm) {
+        return new Promise(function (resolve) {
+
+            var username = process.env.CONFLUENCE_USERNAME,
+                password = process.env.CONFLUENCE_PASSWORD,
+                url = 'http://' + username + ':' + password + '@confluencesite.atlassian.net/wiki/rest/api/content/search?cql=text~' + searchTerm;
+
+            var results = [];
+
+            request(
+                {
+                    method: 'GET',
+                    uri: url
+                }, function (error, response, body) {
+                    if (response.statusCode == 200) {
+                        console.log("Success");
+                        results = JSON.parse(body).results;
+                        // console.log(results);
+                        resolve(results);
+                    } else {
+                        console.log('error: ' + response.statusCode);
+                        console.log(body);
+                    }
+                }
+            );
+        });
+    }
+}
