e494dfadf5dfdeaf71a1036e0e5c822c60197774
Kristin Ottofy <kottofy@outlook.com>
2017-05-26 17:27:48 -0400

    added closing intent dialog and updated if no search results found

--- confluence-search-bot/app.js
+++ confluence-search-bot/app.js
@@ -5,12 +5,11 @@ var builder = require('botbuilder');
 var restify = require('restify');
 var Search = require('./search');
 
-// Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
-// Create connector and listen for messages
+
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
@@ -21,8 +20,6 @@ var bot = new builder.UniversalBot(connector, function (session) {
     session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
 });
 
-// You can provide your own model by specifing the 'LUIS_MODEL_URL' environment variable
-// This Url can be obtained by uploading or creating your model from the LUIS portal: https://www.luis.ai/
 var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
 bot.recognizer(recognizer);
 
@@ -37,20 +34,24 @@ bot.dialog('Search', [
 
             Search.search(searchTerm)
                 .then(function (results) {
-                    var message = new builder.Message()
-                        .attachmentLayout(builder.AttachmentLayout.carousel)
-                        .attachments(results.map(ResultAsAttachment));
 
-                    session.send(message);
-
-                    session.endDialog();
+                    if (results.length>0) {
+                        var message = new builder.Message()
+                            .attachmentLayout(builder.AttachmentLayout.carousel)
+                            .attachments(results.map(ResultAsAttachment));
+                        session.send(message);
+                        session.endDialog();
+                    }
+                    else {
+                        session.endDialog("No results were found. Please try another search.");
+                    }
                 });
         }
     }
 ]).triggerAction({
     matches: 'Search',
     onInterrupted: function (session) {
-        session.send('Sorry I did not understand or something happened. Please ask me to search for something or type \'help\' for assistance.');
+        session.endDialog('Sorry I did not understand or something happened. Please ask me to search for something or type \'help\' for assistance.');
     }
 });
 
@@ -64,6 +65,12 @@ bot.dialog('Greeting', function (session) {
     session.endDialog('Hi! I can help you search Confluence. Try asking me things like \'search for password policy\' or \'find me the email templates\'');
 }).triggerAction({
     matches: 'Greeting'
+});
+
+bot.dialog('Closing', function (session) {
+    session.endDialog('Thank you for the chat! Have a great day!');
+}).triggerAction({
+    matches: 'Closing'
 });
 
 function ResultAsAttachment(result) {
